667670a0f34f3d43caf90f49b2d24138c4794e93
Unknown <maxencebernier@hotmail.fr>
2018-12-05 09:33:28 +0100

    Spring Initilizr
    Spring Initilizr with starter test, mongodb and mongodb embedded

---
+++ src/test/java/com/polytechcloud/polytechcloud/PolytechCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.polytechcloud.polytechcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PolytechCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
