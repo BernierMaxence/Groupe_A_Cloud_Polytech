1a0c964a0417b5aa246ab32b41961298c3f8e662
Maxence Bernier <maxencebernier@hotmail.fr>
2018-12-12 11:40:52 +0100

    @Autowire changed because og @InsertMocks in tests

--- src/main/java/com/polytechcloud/polytechcloud/controller/UserController.java
+++ src/main/java/com/polytechcloud/polytechcloud/controller/UserController.java
@@ -2,37 +2,22 @@ package com.polytechcloud.polytechcloud.controller;
 
 import com.polytechcloud.polytechcloud.entity.User;
 import com.polytechcloud.polytechcloud.repository.UserRepository;
-import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.OrderComparator;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+
 import java.net.URISyntaxException;
-import java.time.Instant;
-import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class UserController {
 
-    private final UserRepository userRepository;
-
     @Autowired
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private UserRepository userRepository;
 
-    /* @GetMapping(path="/")
-    public String get() {
-        return "Hello world";
-    }*/
 
     @GetMapping(path="/user")
     public Iterable<User> getAllUsers() {
@@ -75,7 +60,6 @@ public class UserController {
 
     @DeleteMapping(path = "/user/{id}")
     public void deleteUser(@PathVariable int id) {
-
         userRepository.deleteById(id);
 
     }
