7cfa50ee8ad0794230affe8e5cc3b4c6d228026f
CHABRIER Valentin <chabrier.valentin@gmail.com>
2018-12-29 12:57:55 +0100

    remove skip_cleanup

--- deployCleverCloud.sh
+++ deployCleverCloud.sh
@@ -1,5 +1,4 @@
 echo $key | base64 --decode >> key
 chmod 700 key
-ssh-keygen -R 212.129.46.93
 GIT_SSH_COMMAND='ssh -i key' git push --force git+ssh://git@push-par-clevercloud-customers.services.clever-cloud.com/app_b58bad46-6dd3-49c3-a3c2-26aee5df0565.git master
 rm key
