0c6801888fde853fd374d5ea2ec2c4d4bb84d57e
CHABRIER Valentin <chabrier.valentin@gmail.com>
2018-12-29 13:24:06 +0100

    add ssh -v in deploy script

--- deployCleverCloud.sh
+++ deployCleverCloud.sh
@@ -1,4 +1,4 @@
 echo $key | base64 --decode >> key
 chmod 700 key
-GIT_SSH_COMMAND='ssh -i key' git push --force git+ssh://git@push-par-clevercloud-customers.services.clever-cloud.com/app_b58bad46-6dd3-49c3-a3c2-26aee5df0565.git master
+GIT_SSH_COMMAND='ssh -i -v key' git push --force git+ssh://git@push-par-clevercloud-customers.services.clever-cloud.com/app_b58bad46-6dd3-49c3-a3c2-26aee5df0565.git master
 rm key
